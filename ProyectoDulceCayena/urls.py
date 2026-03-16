526f30a57e86702c73fb7edf3100e0164bbccc11
Estebin-77 <delarosainfante@gmail.com>
2026-03-16 19:50:04 -0400

    Servir archivos media en producción

--- ProyectoDulceCayena/urls.py
+++ ProyectoDulceCayena/urls.py
@@ -18,6 +18,9 @@ urlpatterns = [
 ]
 
 # ✅ Esto permite ver las imágenes y archivos multimedia mientras estás en modo DEBUG
+# Archivos multimedia
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Archivos estáticos solo en desarrollo
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
